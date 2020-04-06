e9afd75f6732c08fb512c05897680b1ae6dd752f
dpthegrey <dpthegrey@gmail.com>
2020-04-07 04:03:36 +0530

    Programming Assignment Q-15

---
+++ programming_assignment/15/main.py
@@ -0,0 +1,17 @@
+class Foo(object):
+    def __init__(self):
+        print("foo init")
+
+
+class Bar(object):
+    def __init__(self):
+        print("bar init")
+
+
+class FooBar(Foo, Bar):
+    def __init__(self):
+        print("foobar init")
+        super(FooBar, self).__init__()
+
+
+a = FooBar()
