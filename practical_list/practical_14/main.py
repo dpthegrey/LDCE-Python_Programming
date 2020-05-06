44352db3efe7f976c60c92bceb52e8527c774d14
dpthegrey <dpthegrey@gmail.com>
2020-05-06 14:58:21 +0530

    Practical List Q-14

---
+++ practical_list/practical_14/main.py
@@ -0,0 +1,10 @@
+import datetime
+
+end = datetime.datetime(2020, 4, 1, 12, 00, 00)
+start = datetime.datetime(1999, 1, 1, 00, 00, 00)
+
+diff = end - start
+
+print('Start: ', start)
+print('End:', end)
+print('Difference:', diff)
