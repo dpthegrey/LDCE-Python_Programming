90a504ab1f82540f66768e7f856d918e0ae3a01e
dpthegrey <dpthegrey@gmail.com>
2020-06-19 22:57:12 +0530

    Key Difference between black-box and glass-box testing

---
+++ previous_year_GTU_question_papers/Nov2019/q4a1.py
@@ -0,0 +1,17 @@
+# Give difference between black-box and glass-box(white-box) testing
+# https://www.guru99.com/back-box-vs-white-box-testing.html
+
+# Key Difference between black box testing and white box(glass box) testing
+"""
+- In Black Box, testing is done without the knowledge of the internal structure of program or application whereas in White Box, testing is done with knowledge of the internal structure of program.
+
+- Black Box test doesn't require programming knowledge whereas the White Box test requires programming knowledge.
+
+- Black Box testing has the main goal to test the behavior of the software whereas White Box testing has the main goal to test the internal operation of the system.
+
+- Black Box testing is focused on external or end-user perspective whereas the White Box testing is focused on code structure, conditions, paths and branches.
+
+- Black Box test provides low granularity reports whereas the White Box test provides high granularity reports.
+
+- Black Box testing is a not time-consuming process whereas White Box testing is a time-consuming process.
+"""
