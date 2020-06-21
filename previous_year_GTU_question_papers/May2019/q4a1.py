e055b2216267ad7237dc82cd48569ce3251f3210
dpthegrey <dpthegrey@gmail.com>
2020-06-22 03:24:32 +0530

    Defining socket functions: socket(), bind() and listen()

---
+++ previous_year_GTU_question_papers/May2019/q4a1.py
@@ -0,0 +1,13 @@
+"""
+socket.socket()
+socket() function returns a socket object whose methods implement the various socket system calls.
+
+socket.bind()
+This method binds the address(hostname, port number) to the socket.
+
+socket.listen()
+This method basically listens to the connections made to the socket. 
+It starts TCP listener. 
+Backlog is an argument of this method which specifies the maximum number of queued connections. 
+Its minimum value is 0 and maximum value is 5.
+"""
