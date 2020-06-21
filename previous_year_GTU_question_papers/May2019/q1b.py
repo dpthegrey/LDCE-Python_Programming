be108553a7cb8cad3ec5d75aff0d68632d92ab32
dpthegrey <dpthegrey@gmail.com>
2020-06-21 23:49:00 +0530

    concept of specification

---
+++ previous_year_GTU_question_papers/May2019/q1b.py
@@ -0,0 +1,5 @@
+"""
+The central idea of Specification is to seperate the statement of how to match the candidate, 
+from the candidate object that is matched against. 
+As well as its usefulness in selection, it is also valuable for validation and for building to order.
+"""
