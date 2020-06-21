1d44f15f3783bf848e2908ce9cf8ff68b99c1e29
dpthegrey <dpthegrey@gmail.com>
2020-06-22 03:51:15 +0530

    simple python program that draws a line graph using matplotlib.pyplot

---
+++ previous_year_GTU_question_papers/May2019/q4b1/main.py
@@ -0,0 +1,22 @@
+
+# importing the required module
+import matplotlib.pyplot as plt
+
+# x axis values
+x = [1, 2, 3, 4]
+# corresponding y axis values
+y = [1, 4, 9, 16]
+
+# plotting the points
+plt.plot(x, y)
+
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+
+# giving a title to my graph
+plt.title('Q.4 (b)')
+
+# function to show the plot
+plt.show()
